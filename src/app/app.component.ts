73edc47faa47e9060fd65fed504eb4ecf91ec555
WardaArshad <90782818+WardaArshad@users.noreply.github.com>
2024-10-05 00:19:34 +0500

    pipe filters in for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CalculateComponent } from './calculate/calculate.component';
 import { PipePracticeComponent } from "./pipe-practice/pipe-practice.component";
+import { PipeComponent } from "./pipe-practice/pipe/pipe.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CalculateComponent, PipePracticeComponent],
+  imports: [RouterOutlet, HeaderComponent, CalculateComponent, PipePracticeComponent, PipeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pipe-practice/pipe/pipe.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { ShortenPipe } from '../shorten.pipe';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgFor } from '@angular/common';
+import { FilterPipe } from '../filter.pipe';
+
+@Component({
+  selector: 'app-pipe',
+  standalone: true,
+  imports: [ShortenPipe, FilterPipe, FormsModule, CommonModule, NgFor],
+  templateUrl: './pipe.component.html',
+  styleUrl: './pipe.component.scss'
+})
+export class PipeComponent {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  filteredStatus = '';
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
+
+  onAddServer(){
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
+  }
+
+}
